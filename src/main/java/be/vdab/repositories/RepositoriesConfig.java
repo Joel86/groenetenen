0b97d7aa8f89e41319e8c772a0f2c438181ef9a9
Joel Vanherle <joel.vanherle@hotmail.com>
2017-11-27 15:59:11 +0100

    Converted repositories to spring-data

--- src/main/java/be/vdab/repositories/RepositoriesConfig.java
+++ src/main/java/be/vdab/repositories/RepositoriesConfig.java
@@ -3,9 +3,9 @@ package be.vdab.repositories;
 import javax.sql.DataSource;
 
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.dao.annotation.PersistenceExceptionTranslationPostProcessor;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.orm.jpa.JpaTransactionManager;
 import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
 import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
@@ -14,13 +14,8 @@ import be.vdab.entities.Filiaal;
 import be.vdab.valueobjects.Adres;
 
 @Configuration
-@ComponentScan
+@EnableJpaRepositories
 public class RepositoriesConfig {
-	@Bean
-	JpaTransactionManager transactionManager(
-			LocalContainerEntityManagerFactoryBean entityManagerFactory) {
-		return new JpaTransactionManager(entityManagerFactory.getObject());
-	}
 	@Bean
 	LocalContainerEntityManagerFactoryBean entityManagerFactory(DataSource dataSource) {
 		LocalContainerEntityManagerFactoryBean factory = 
@@ -34,6 +29,11 @@ public class RepositoriesConfig {
 		factory.getJpaPropertyMap().put("hibernate.format_sql", true);
 		factory.getJpaPropertyMap().put("hibernate.use_sql_comments", true);
 		return factory;
+	}
+	@Bean
+	JpaTransactionManager transactionManager(
+			LocalContainerEntityManagerFactoryBean entityManagerFactory) {
+		return new JpaTransactionManager(entityManagerFactory.getObject());
 	}
 	@Bean
 	PersistenceExceptionTranslationPostProcessor persistenceExceptionTranslator() {

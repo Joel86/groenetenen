7c1cadfcc515da00f72bf739babdc3f55c6d4d4a
Joel Vanherle <joel.vanherle@hotmail.com>
2017-12-04 14:18:22 +0100

    Added Spring security login restrictions

--- src/main/java/be/vdab/web/ControllersConfig.java
+++ src/main/java/be/vdab/web/ControllersConfig.java
@@ -49,6 +49,7 @@ public class ControllersConfig extends WebMvcConfigurerAdapter {
 	@Override
 	public void addViewControllers(ViewControllerRegistry registry) {
 		registry.addViewController("/info").setViewName("info");
+		registry.addViewController("/login").setViewName("login");
 	}
 	@Bean
 	LocaleResolver localeResolver() {
--- src/main/java/be/vdab/web/Initializer.java
+++ src/main/java/be/vdab/web/Initializer.java
@@ -3,7 +3,6 @@ package be.vdab.web;
 import javax.servlet.Filter;
 
 import org.springframework.orm.jpa.support.OpenEntityManagerInViewFilter;
-import org.springframework.web.filter.CharacterEncodingFilter;
 import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
 
 import be.vdab.datasource.DataSourceConfig;
@@ -12,6 +11,7 @@ import be.vdab.mail.MailConfig;
 import be.vdab.repositories.RepositoriesConfig;
 import be.vdab.restclients.RestClientsConfig;
 import be.vdab.restservices.RestControllersConfig;
+import be.vdab.security.SecurityConfig;
 import be.vdab.services.ServicesConfig;
 
 public class Initializer 
@@ -24,7 +24,7 @@ public class Initializer
 	protected Class<?>[] getRootConfigClasses() {
 		return new Class<?>[] {RepositoriesConfig.class, ServicesConfig.class, 
 			DataSourceConfig.class, RestClientsConfig.class, MailConfig.class, 
-			JmsConfig.class};
+			JmsConfig.class, SecurityConfig.class};
 	}
 	@Override
 	protected Class<?>[] getServletConfigClasses() {
@@ -32,7 +32,6 @@ public class Initializer
 	}
 	@Override
 	protected Filter[] getServletFilters() {
-		return new Filter[] {new CharacterEncodingFilter("UTF-8"), 
-				new OpenEntityManagerInViewFilter()};
+		return new Filter[] {new OpenEntityManagerInViewFilter()};
 	}
 }

bd20b39b98914b7963b4566551cf242c7d3aa067
Joel Vanherle <joel.vanherle@hotmail.com>
2017-12-04 08:58:53 +0100

    Added Ajax request filiaal zoeken per id

--- src/main/java/be/vdab/web/FiliaalController.java
+++ src/main/java/be/vdab/web/FiliaalController.java
@@ -43,6 +43,7 @@ class FiliaalController {
 			"redirect:/filialen/{id}?optimisticlockingexception=true";
 	private static final String AFSCHRIJVEN_VIEW = "filialen/afschrijven";
 	private static final String REDIRECT_NA_AFSCHRIJVEN = "redirect:/";
+	private static final String PER_ID_VIEW = "filialen/perid";
 	private final FiliaalService filiaalService;
 	FiliaalController(FiliaalService filiaalService) {
 		this.filiaalService = filiaalService;
@@ -145,6 +146,10 @@ class FiliaalController {
 		}
 		filiaalService.afschrijven(afschrijvenForm.getFilialen());
 		return new ModelAndView(REDIRECT_NA_AFSCHRIJVEN);
+	}
+	@GetMapping("perid")
+	String findById() {
+		return PER_ID_VIEW;
 	}
 	@InitBinder("postcodeReeks")
 	void initBinderPostcodeReeks(WebDataBinder binder) {

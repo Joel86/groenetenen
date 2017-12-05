27afd3929415803709e190cede67b2df8b2f00e3
Joel Vanherle <joel.vanherle@hotmail.com>
2017-12-05 10:56:18 +0100

    Added AOP auditing, logging, performance and statistics

--- src/main/java/be/vdab/web/Initializer.java
+++ src/main/java/be/vdab/web/Initializer.java
@@ -5,6 +5,7 @@ import javax.servlet.Filter;
 import org.springframework.orm.jpa.support.OpenEntityManagerInViewFilter;
 import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
 
+import be.vdab.aop.AopConfig;
 import be.vdab.datasource.DataSourceConfig;
 import be.vdab.jms.JmsConfig;
 import be.vdab.mail.MailConfig;
@@ -24,7 +25,7 @@ public class Initializer
 	protected Class<?>[] getRootConfigClasses() {
 		return new Class<?>[] {RepositoriesConfig.class, ServicesConfig.class, 
 			DataSourceConfig.class, RestClientsConfig.class, MailConfig.class, 
-			JmsConfig.class, SecurityConfig.class};
+			JmsConfig.class, SecurityConfig.class, AopConfig.class};
 	}
 	@Override
 	protected Class<?>[] getServletConfigClasses() {
